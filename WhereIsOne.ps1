a2789cffe4a060f1cb7418f82059df7ac8d6b8bb
Jose A Lerma III <JoseALermaIII@users.noreply.github.com>
2016-01-27 02:19:28 -0600

    Create WhereIsOne.ps1

---
+++ WhereIsOne.ps1
@@ -0,0 +1,17 @@
+## WhereIsOne.ps1
+## Gives the location of the number 1 in an array
+##
+## Author:   J. Lerma
+## Date:     December 12, 2015
+##
+$input = 0,2,3,1
+$var_b = 0
+
+foreach ($var_a in $input)
+{
+    if ($var_a -eq "1")
+    {
+        return $var_b
+    }
+    $var_b++
+}
